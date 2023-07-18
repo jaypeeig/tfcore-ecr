89de44110b6ccfe3572203272b741d0327d6b933
Jaypee <jignacio@cambridge.org>
2023-07-18 20:31:13 +0800

    Test be

--- main.tf
+++ main.tf
@@ -7,3 +7,14 @@ terraform {
   }
   required_version = "~> 1.4.4"
 }
+
+provider "aws" {
+  region                      = "us-east-1"
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  skip_metadata_api_check     = true
+  s3_force_path_style         = true
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
+
+}
